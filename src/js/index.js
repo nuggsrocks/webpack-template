43b98a24d7852da8246b4baa0424f10d7aceef61
mattfisher <nuggsrocks@yahoo.com>
2021-05-17 02:51:30 -0400

    initial commit

---
+++ src/js/index.js
@@ -0,0 +1,7 @@
+import 'core-js/stable';
+import 'regenerator-runtime/runtime';
+import '../scss/index.scss';
+
+let div = document.querySelector('div#root');
+
+div.innerHTML = '<h1>Hello world</h1>';
---
+++ webpack.dev.js
@@ -0,0 +1,19 @@
+const {merge} = require('webpack-merge');
+const commonConfig = require('./webpack.common.js');
+
+module.exports = merge(commonConfig, {
+	mode: 'development',
+	devtool: 'inline-source-map',
+	module: {
+		rules: [
+			{
+				test: /\.css$/,
+				use: ['style-loader', 'css-loader']
+			},
+			{
+				test: /\.scss$/,
+				use: ['style-loader', 'css-loader', 'sass-loader']
+			},
+		]
+	}
+});
